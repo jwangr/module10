5a9219c98d450803af2cd41c63ff4b2b8f58fb65
ThinkCentre <jwangr@ymail.com>
2025-09-27 08:35:42 +1000

    Changed base name

--- lab-module6/vite.config.js
+++ lab-module6/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jwangr.github.io/module10/lab-module6"
+  base: 'module10'
 })
